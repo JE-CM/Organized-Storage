e9a492268cfe95fe7b84e954089c1b8094b6f522
matthew-silva <matthewsilva18@icloud.com>
2024-09-17 22:54:54 -0400

    USER STORY 14 : Create database functions specific to organized storage user management (#7)
    * Added some db functions specific for organized storage user management
    
    * Adding remove user and unit tests to Organized Storage Database class
    
    * Added update user method
    
    * OrganizedStorageDatabase unit tests are working now, allowing creating, removing, updating, and deleting users. Big design improvements are still needed to make the fucntions return useful info, but they are a minimal viable example of user management
    
    * Added TODOs from PR as additional user stories

---
+++ src/database/tests/OrganizedStorageDatabase_unittest.py
@@ -0,0 +1,72 @@
+import unittest
+import sys
+import os
+
+# Add the directory containing the OrganizedStorageDatabase package to the system path
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+# Import the class to be tested
+from OrganizedStorageDatabase import OrganizedStorageDatabase
+
+
+class TestOrganizedStorageDatabase(unittest.TestCase):
+    def setUp(self):
+        self.test_db = OrganizedStorageDatabase('.','organized_storage_unittest.db')
+        self.test_user = {
+            "name": 'Alice',
+            "address": '30 Giovanni Ln, Atlantis, 12345',
+            "hashed_password": 'TODO_HASHED_PASSWORD',
+            "password_salt": 'TODO_PASSWORD_SALT',
+            "square_meters_used": 8.5,
+        }
+        self.test_user_edited = dict(self.test_user)
+        self.test_user_edited["name"] = "Bob"
+
+    def tearDown(self):
+        self.test_db.delete_db()
+
+    def test_add_user(self):
+        add_result, id = self.test_db.add_user(self.test_user)
+        self.assertEqual(add_result, True)
+        self.assertEqual(id, 1)
+
+    def test_user_search(self):
+        key_to_search = "name"
+        self.test_db.add_user(self.test_user)
+        results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
+        self.assertEqual(len(results), 1)
+        primary_key_column_number = self.test_db.get_primary_key_column_number()
+        self.assertEqual(results[0][primary_key_column_number], 1)
+        # TODO: Need to convert user_search to convert tuple results to dictionaries instead
+        # self.assertEqual(results[0][key_to_search], self.test_user[key_to_search])
+        
+
+    def test_remove_user(self):
+        self.test_db.add_user(self.test_user)
+        key_to_search = "name"
+        results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
+        self.assertEqual(len(results), 1)
+        # TODO: remove_user does not return anything meaningful yet because the SqliteDatabase.execute_query
+        #       function does not return anything that a DELETE query would use for output, such as cursor.rowcount
+        #       . We should convert SqliteDatabase.execute_query to return a dict with all possible values
+        _, _ = self.test_db.remove_user(key_to_search, self.test_user[key_to_search])
+        after_remove_results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
+        self.assertEqual(len(after_remove_results), 0)
+
+    def test_user_search_on_nonexistent_user(self):
+        key_to_search = "name"
+        results= self.test_db.user_search(key_to_search, self.test_user[key_to_search])
+        self.assertEqual(len(results), 0)
+
+    def test_update_user_by_id(self):
+        _, id = self.test_db.add_user(self.test_user)
+        update_result, id = self.test_db.update_user_by_id(id, self.test_user_edited)
+        key_to_search = "name"
+        results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
+        self.assertEqual(len(results), 0)
+        key_to_search = "name"
+        results = self.test_db.user_search(key_to_search, self.test_user_edited[key_to_search])
+        self.assertEqual(len(results), 1)
+        
+if __name__ == "__main__":
+    unittest.main()
