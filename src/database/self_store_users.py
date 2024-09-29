10e09818135a1a558b97acef411507b86a00fdb7
Matthew Silva <matthewsilva18@icloud.com>
2024-09-29 16:41:21 -0400

    Operations to add units to uses now work and are tested. Need to add functionality to remove units now

--- src/database/self_store_users.py
+++ src/database/self_store_users.py
@@ -11,6 +11,7 @@ class SelfStoreUserDatabase(OrganizedStorageDatabase):
     # well as a reference to the storage unit(s) being used by the customer and the
     # price the customer pays for those units.
     SELF_STORE_UNIT_LIST_NAME = "storage_unit_list"
+    SELF_STORE_UNIT_PRICE_LIST_NAME = "storage_unit_prices"
     SELF_STORE_USERS_DB_COLUMNS = {
         "id": {
             "primary": True,
@@ -39,7 +40,7 @@ class SelfStoreUserDatabase(OrganizedStorageDatabase):
             "type": "TEXT",
         },
         # List of prices paid for units in the order of storage_unit_list, separated by commas
-        "storage_unit_prices": {
+        SELF_STORE_UNIT_PRICE_LIST_NAME: {
             "type": "TEXT",
         }
     }
@@ -48,15 +49,25 @@ class SelfStoreUserDatabase(OrganizedStorageDatabase):
         super().__init__(db_dir,self_store_users_db_filename, self.SELF_STORE_USERS_DB_COLUMNS)
 
 
-    # TODO, change units_to_add and prices_to_add to a dictionary instead
-    def add_units_to_user(self, user_id, units_to_add, prices_to_add):
+    # TODO:
+    # def remove_units_from_user():
+
+    def add_units_to_user(self, user_id, unit_dict_list):
+        units_to_add = [unit['id'] for unit in unit_dict_list]
+        prices_of_units_to_add = [unit['price'] for unit in unit_dict_list]
         units = self.get_self_store_unit_list_of_user(user_id)
-        print(f"units == {units}")
+        unit_prices = self.get_self_store_unit_price_list_of_user(user_id)
+        intersection_of_units_to_add_and_units = []
+        for to_add in units_to_add:
+            if to_add in units:
+                intersection_of_units_to_add_and_units.append(to_add)
+        if len(intersection_of_units_to_add_and_units) != 0:
+            raise Exception(f"Request to add unit(s) {intersection_of_units_to_add_and_units} invalid. They already exist in the unit list for user {user_id}") 
         units.extend(units_to_add)
-        print(f"units == {units}")
+        unit_prices.extend(prices_of_units_to_add)
         units_to_update = dict()
         units_to_update[self.SELF_STORE_UNIT_LIST_NAME] = ",".join([str(n) for n in units])
-        units_to_update[self.SELF_STORE_UNIT_PRICE_LIST_NAME] = .
+        units_to_update[self.SELF_STORE_UNIT_PRICE_LIST_NAME] = ",".join([str(n) for n in unit_prices])
         return self.update_user_by_id(user_id, units_to_update)
 
     def get_self_store_unit_dict_of_user(self, user_id):
@@ -64,17 +75,17 @@ class SelfStoreUserDatabase(OrganizedStorageDatabase):
         if len(results) == 0:
             return dict()
         assert(len(results) == 1)
-        print(f"results == {results}")
         self_store_unit_list_of_user = [] if results[0]["storage_unit_list"] == '' else results[0]["storage_unit_list"].split(",")
         self_store_unit_price_list_of_user = [] if results[0]["storage_unit_prices"] == '' else results[0]["storage_unit_prices"].split(",")
-        print(f"self_store_unit_list_of_user == {self_store_unit_list_of_user}")
-        print(f"self_store_unit_price_list_of_user == {self_store_unit_price_list_of_user}")
         return dict(zip(self_store_unit_list_of_user, self_store_unit_price_list_of_user))
 
     def get_self_store_unit_list_of_user(self, user_id):
         unit_dict = self.get_self_store_unit_dict_of_user(user_id)
-        print(f"unit_dict == {unit_dict}")
         return list(unit_dict.keys())
+
+    def get_self_store_unit_price_list_of_user(self, user_id):
+        unit_dict = self.get_self_store_unit_dict_of_user(user_id)
+        return list(unit_dict.values())
 
     def user_authorized_to_access_unit(self, unit_id, user_id):
         return unit_id in self.get_self_store_unit_list_of_user(user_id)
@@ -82,3 +93,4 @@ class SelfStoreUserDatabase(OrganizedStorageDatabase):
     # TODO: username must be unique, need to implement as another primary column
     def authenticate_user(self, username, password):
         print("TODO")
+        
--- src/database/tests/self_store_users_unittest.py
+++ src/database/tests/self_store_users_unittest.py
@@ -32,42 +32,22 @@ class TestSelfStoreUserDatabase(unittest.TestCase):
         print("This test case doesn't test anything except the setUp and tearDown functions")
 
     def test_self_store_add_user(self):
-        self.test_self_store_users_db.add_user(self.test_self_store_user)
-    
+        result, id_added = self.test_self_store_users_db.add_user(self.test_self_store_user)
+        self.assertEqual(result, True)
+        self.assertEqual(id_added, 1)
+
     def test_self_store_remove_user(self):
         self.test_self_store_users_db.add_user(self.test_self_store_user)
         key_to_search = "name"
-        self.test_self_store_users_db.remove_user(key_to_search, self.test_self_store_user[key_to_search])
-        
+        result = self.test_self_store_users_db.remove_user(key_to_search, self.test_self_store_user[key_to_search])
+        self.assertEqual(result, True)
+
     def test_self_store_search_user(self):
         self.test_self_store_users_db.add_user(self.test_self_store_user)
         key_to_search = "name"
         results = self.test_self_store_users_db.user_search(key_to_search, self.test_self_store_user[key_to_search])
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0][key_to_search], self.test_self_store_user[key_to_search])
-
-    # def test_user_search(self):
-    #     key_to_search = "name"
-    #     self.test_db.add_user(self.test_user)
-    #     results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
-    #     self.assertEqual(len(results), 1)
-    #     self.assertEqual(results[0]['id'], 1)
-    #     self.assertEqual(results[0][key_to_search], self.test_user[key_to_search])
-
-    # def test_remove_user(self):
-    #     self.test_db.add_user(self.test_user)
-    #     key_to_search = "name"
-    #     results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
-    #     self.assertEqual(len(results), 1)
-    #     removed_user = self.test_db.remove_user(key_to_search, self.test_user[key_to_search])
-    #     self.assertEqual(removed_user, True)
-    #     after_remove_results = self.test_db.user_search(key_to_search, self.test_user[key_to_search])
-    #     self.assertEqual(len(after_remove_results), 0)
-
-    # def test_user_search_on_nonexistent_user(self):
-    #     key_to_search = "name"
-    #     results= self.test_db.user_search(key_to_search, self.test_user[key_to_search])
-    #     self.assertEqual(len(results), 0)
 
     def test_update_user_by_id(self):
         _, id = self.test_self_store_users_db.add_user(self.test_self_store_user)
@@ -81,29 +61,106 @@ class TestSelfStoreUserDatabase(unittest.TestCase):
 
     def test_add_units_to_user(self):
         _, id = self.test_self_store_users_db.add_user(self.test_self_store_user)
-        new_unit_list = [1,5,999]
-        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_list)
+        before_results = self.test_self_store_users_db.user_search(self.test_self_store_users_db.get_primary_key(), id)
+        self.assertEqual(len(before_results), 1)
+        self.assertEqual(before_results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_LIST_NAME], "")
+        new_unit_dict_list = [
+            {
+                "id": "1",
+                "price": "300",
+            },
+            {
+                "id": "25",
+                "price": "100",
+            },
+            {
+                "id": "234",
+                "price": "145",
+            },
+        ]
+        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_dict_list)
         self.assertEqual(add_unit_result, True)
         results = self.test_self_store_users_db.user_search(self.test_self_store_users_db.get_primary_key(), id)
         self.assertEqual(len(results), 1)
-        self.assertEqual(results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_LIST_NAME], ",".join([str(n) for n in new_unit_list]))
+        self.assertEqual(results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_LIST_NAME], ",".join([n['id'] for n in new_unit_dict_list]))
+        self.assertEqual(results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_PRICE_LIST_NAME], ",".join([n['price'] for n in new_unit_dict_list]))
 
     def test_add_units_to_user_that_does_not_exist(self):
-        new_unit_list = [1,5,999]
-        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_list)
+        new_unit_dict_list = [
+            {
+                "id": "1",
+                "price": "300",
+            },
+            {
+                "id": "25",
+                "price": "100",
+            },
+            {
+                "id": "234",
+                "price": "145",
+            },
+        ]
+        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_dict_list)
         self.assertEqual(add_unit_result, False)
         results = self.test_self_store_users_db.user_search(self.test_self_store_users_db.get_primary_key(), id)
+        self.assertEqual(len(results), 0)
+
+    def test_add_units_to_user_that_already_has_them(self):
+        _, id = self.test_self_store_users_db.add_user(self.test_self_store_user)
+        before_results = self.test_self_store_users_db.user_search(self.test_self_store_users_db.get_primary_key(), id)
+        self.assertEqual(len(before_results), 1)
+        self.assertEqual(before_results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_LIST_NAME], "")
+        new_unit_dict_list = [
+            {
+                "id": "1",
+                "price": "300",
+            },
+            {
+                "id": "25",
+                "price": "100",
+            },
+            {
+                "id": "234",
+                "price": "145",
+            },
+        ]
+        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_dict_list)
+        self.assertEqual(add_unit_result, True)
+        error_on_unit_add = False
+        try:
+            add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_dict_list)
+        except:
+            error_on_unit_add = True
+        self.assertEqual(error_on_unit_add, True)
+        results = self.test_self_store_users_db.user_search(self.test_self_store_users_db.get_primary_key(), id)
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_LIST_NAME], ",".join([n['id'] for n in new_unit_dict_list]))
+        self.assertEqual(results[0][SelfStoreUserDatabase.SELF_STORE_UNIT_PRICE_LIST_NAME], ",".join([n['price'] for n in new_unit_dict_list]))
 
     def test_user_authorized_to_access_unit(self):
         _, id = self.test_self_store_users_db.add_user(self.test_self_store_user)
-        new_unit_list = [1,5,999]
-        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_list)
+        new_unit_dict_list = [
+            {
+                "id": "1",
+                "price": "300",
+            },
+            {
+                "id": "25",
+                "price": "100",
+            },
+            {
+                "id": "234",
+                "price": "145",
+            },
+        ]
+        add_unit_result = self.test_self_store_users_db.add_units_to_user(id, new_unit_dict_list)
         self.assertEqual(add_unit_result, True)
-        authorized = self.test_self_store_users_db.user_authorized_to_access_unit(new_unit_list[1], id)
+        authorized = self.test_self_store_users_db.user_authorized_to_access_unit(new_unit_dict_list[1]['id'], id)
         self.assertEqual(authorized, True)
         authorized = self.test_self_store_users_db.user_authorized_to_access_unit(24, id)
         self.assertEqual(authorized, False)
-        
+
+    # TODO:
     # def test_check_user_auth
 
 if __name__ == "__main__":
